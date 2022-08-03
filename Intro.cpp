c15e08b14e4ed0592527e2732dd2658b224a91ad
BASAVARAJ HADIMANI <106008685+basavarajh99@users.noreply.github.com>
2022-08-03 12:35:29 +0530

    Update Intro.cpp

--- Intro.cpp
+++ Intro.cpp
@@ -3,3 +3,27 @@ Complete BT : All the levels are completely filled except the last level; In the
 Perfect BT : All leaf nodes are at same level
 Balanced BT : Height of tree at max log(N)
 Degenerate tree : every node has single children
+
+representation in c++;
+
+struct Node 
+{
+  int data;
+  struct Node* left;
+  struct Node* right;
+  
+  Node(int val)
+  {
+    data = val;
+    left = right = NULL;
+  }
+};
+
+int main()
+{
+  struct Node* root = new Node(1);
+  root->left = new Node(2);
+  root->right = new Node(3);
+}
+
+
