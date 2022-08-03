853ef951dff2a8e5c1785412c7307e5359e35b13
BASAVARAJ HADIMANI <106008685+basavarajh99@users.noreply.github.com>
2022-08-03 12:26:03 +0530

    Create Intro.cpp

---
+++ Intro.cpp
@@ -0,0 +1,5 @@
+Full BT : Every node has either 0 or 2 nodes.
+Complete BT : All the levels are completely filled except the last level; In the last level all the nodes are as left as possible.
+Perfect BT : All leaf nodes are at same level
+Balanced BT : Height of tree at max log(N)
+Degenerate tree : every node has single children
