003d8349bfada277981c2de63b107ec68fc510bf
BASAVARAJ HADIMANI <106008685+basavarajh99@users.noreply.github.com>
2022-08-03 13:15:15 +0530

    Create Postorder-iterative.cpp

---
+++ Postorder-iterative.cpp
@@ -0,0 +1,29 @@
+//using 2 stacks
+vector<int> postorder(Node* root)
+{
+  vector<int> post;
+  if(!root) return post;
+  
+  stack<Node*> st1, st2;
+  st1.push(root);
+  
+  while(!st1.empty())
+  {
+    root = st1.top();
+    st1.pop();
+    st2.push(root);
+    if(root->left)
+      st2.push(root->left);
+    if(root->right)
+      st2.push(root->right);
+  }
+  
+  while(!st2.empty())
+  {
+    post.push_back(st2.top()->val);
+    st2.pop();
+  }
+  
+  return post;
+  
+}
