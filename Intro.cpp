3e3d589cea1e4a13c43a2b9351b6dbb5621890d5
BASAVARAJ HADIMANI <106008685+basavarajh99@users.noreply.github.com>
2022-08-04 21:24:13 +0530

    Update Intro.cpp

--- Intro.cpp
+++ Intro.cpp
@@ -1,10 +1,10 @@
-Full BT : Every node has either 0 or 2 nodes.
-Complete BT : All the levels are completely filled except the last level; In the last level all the nodes are as left as possible.
-Perfect BT : All leaf nodes are at same level
-Balanced BT : Height of tree at max log(N)
-Degenerate tree : every node has single children
+// Full BT : Every node has either 0 or 2 nodes.
+// Complete BT : All the levels are completely filled except the last level; In the last level all the nodes are as left as possible.
+// Perfect BT : All leaf nodes are at same level
+// Balanced BT : Height of tree at max log(N)
+// Degenerate tree : every node has single children
 
-representation in c++;
+// representation in c++;
 
 struct Node 
 {
@@ -26,6 +26,9 @@ int main()
   root->right = new Node(3);
 }
 
-Traversal Techmiques :
-  1) DFS (Inorder, Preorder, Postorder)
-  2) BFS (Levelorder);  
+// Traversal Techmiques :
+//   1) DFS (Inorder, Preorder, Postorder)
+//   2) BFS (Levelorder);  
+
+// To construct a unique binary tree pre-order and post-order only are not enough cuz u can construct different bt for same values
+// U need In-order with any of the other two to construct unique BT
